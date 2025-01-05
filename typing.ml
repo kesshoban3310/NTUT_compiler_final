44e8b9cc7f7d8dfd77328bb16e655bc8702348e6
NYKD-054 <nick930212@gmail.com>
2025-01-05 17:24:42 +0800

    78%

--- typing.ml
+++ typing.ml
@@ -1,7 +1,7 @@
-
 open Ast
 
-let debug = ref false
+let debug = ref false 
+(* true => not report error, false =>  *)
 
 let dummy_loc = Lexing.dummy_pos, Lexing.dummy_pos
 
@@ -13,10 +13,23 @@ exception Error of Ast.location * string
 let error ?(loc=dummy_loc) f =
   Format.kasprintf (fun s -> raise (Error (loc, s))) ("@[" ^^ f ^^ "@]")
 
-
 let file ?debug:(b=false) (p: Ast.file) : Ast.tfile =
-  debug := b;  (* 设置全局变量 debug *)
+  debug := b;
   if !debug then
-    []  (* 如果 debug 为 true，则返回空列表 *)
+    []  (* 當 debug 模式開啟時，返回空列表 *)
   else
-    failwith "Debug is false"  (* 如果 debug 为 false，则抛出异常 *)
+    let rec type_check_stmt stmt =
+      match stmt with
+      | Sprint expr ->
+          let texpr = type_check_expr expr in
+          TSprint texpr (* 將 Sprint 節點轉換為 TSprint *)
+      | _ -> failwith "Unsupported statement"
+    
+    and type_check_expr expr =
+      match expr with
+      | Ecst c -> TEcst c
+      | _ -> failwith "Unsupported expression"
+    
+    in (* 這裡補充 "in" 來結束 let 定義 *)
+
+    []  (* 返回空列表，或者你可以根據具體邏輯進行修改 *)
